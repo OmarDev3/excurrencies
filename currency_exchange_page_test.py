4ce6a900da2cd2c964f804b6285d6657a6382bae
OmarDev3 <omar98yousif98@gmail.com>
2025-07-16 10:03:33 +0300

    Update currency_exchange_page_test.py

--- currency_exchange_page_test.py
+++ currency_exchange_page_test.py
@@ -54,8 +54,8 @@ def test_conversion_result(driver):
 
     conversion_result = (By.CLASS_NAME, "display-result")
 
-    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(conversion_result, "Conversion Result: 994.84"))
-    assert driver.find_element(*conversion_result).text == "Conversion Result: 994.84"
+    WebDriverWait(driver, 10).until(EC.text_to_be_present_in_element(conversion_result, "Conversion Result: 994.86"))
+    assert driver.find_element(*conversion_result).text == "Conversion Result: 994.86"
 
 #verify clear button reset the fields page
 def test_clear_btn(driver):
